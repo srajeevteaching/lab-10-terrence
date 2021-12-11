b74a9ede0dae52bba06f5093c57b38a2cb5a607f
TerrenceObuah <90399261+TerrenceObuah@users.noreply.github.com>
2021-12-10 19:45:16 -0500

    done, but it says error.

--- .github/labb10.py
+++ .github/labb10.py
@@ -15,65 +15,48 @@ MORSE_CODE_DICT= {'A': '.-', 'B': '-...',
                    '(': '-.--.', ')': '-.--.-'}
 
 
-# Function to encrypt the string
-# according to the morse code chart
 def encrypt(message):
         cipher = ''
         for letter in message:
                 if letter != ' ':
 
-                        # Looks up the dictionary and adds the
-                        # correspponding morse code
-                        # along with a space to separate
-                        # morse codes for different characters
+
                         cipher += MORSE_CODE_DICT[letter] + ' '
                 else:
-                        # 1 space indicates different characters
-                        # and 2 indicates different words
+
                         cipher += ' '
 
         return cipher
 
 
-# Function to decrypt the string
-# from morse to english
 def decrypt(message):
-        # extra space added at the end to access the
-        # last morse code
+
         message += ' '
 
         decipher = ''
         citext = ''
         for letter in message:
 
-                # checks for space
+
                 if (letter != ' '):
 
-                        # counter to keep track of space
+
                         i = 0
 
-                        # storing morse code of a single character
                         citext += letter
 
-                # in case of space
                 else:
-                        # if i = 1 that indicates a new character
                         i += 1
 
-                        # if i = 2 that indicates a new word
                         if i == 2:
 
-                                # adding space to separate words
                                 decipher += ' '
                         else:
 
-                                # accessing the keys using their values (reverse of encryption)
-                                decipher += list(MORSE_CODE_DICT.keys())[list(MORSE_CODE_DICT
-                                                                              .values()).index(citext)]
+                                decipher += list(MORSE_CODE_DICT.keys())[list(MORSE_CODE_DICT.values()).index(citext)]
                                 citext = ''
 
         return decipher
-
 
 
 def main():
